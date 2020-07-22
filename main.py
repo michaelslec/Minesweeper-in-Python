2ade93c33426bde373266fe455bebb5136b8490c
Michael LeCompte <michaelslec98@gmail.com>
2020-07-21 23:52:37 -0700

    Refactored code into main

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
 
-pygame.init()
+def main():
+    crashed = False
+    while not crashed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
 
-gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption('Minesweeper!')
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print(event)
 
-crashed = False
+        pygame.display.update()
 
-while not crashed:
+    pygame.quit()
+    quit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print(event)
+if __name__ == "__main__":
+    pygame.init()
 
-    pygame.display.update()
+    gameDisplay = pygame.display.set_mode((800,600))
+    pygame.display.set_caption('Minesweeper!')
 
-pygame.quit()
-quit()
+
+    main()
